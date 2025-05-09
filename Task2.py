1df13857efa543a9e5116ffb2b17173f841da79e
Sara Miller <vedhapadhalni@gmail.com>
2025-05-09 20:24:25 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,9 +1,6 @@
-a  = int(input("Enter a number : "))
-import math
-b = math.sqrt(a)
-c = math.log(a)
-d = math.sin(a)
-
-print(b)
-print(c)
-print(d)
+L1 = [1,2,3,4,5,6,7,8,9,10]
+print("Original list : ", L1)
+L2 = L1[0:5]
+print("First five elements : ", L2)
+L2.reverse()
+print("Reversed extracted elements : ", L2)
