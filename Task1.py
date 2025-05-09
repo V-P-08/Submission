b16cf194f88fb77df314de3a2f98db5cca35da0f
Sara Miller <vedhapadhalni@gmail.com>
2025-05-09 20:19:37 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,8 +1,7 @@
-def factorial(a):
-  if a<2:
-    return 1
-  else :
-    return (a*factorial(a-1))
-
-s = factorial(5)
-print(s)
+try :
+    List1 = {'Alice':89,'Sarah':78,'Liam':95,'Badger':87,'Kyle':77,'Marco':99,'Katie':99}
+    name = input("Enter student's name: ")
+    print(List1[name])
+except :
+    KeyError
+    print("Student not found")
